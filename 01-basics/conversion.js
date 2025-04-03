06d20447ea0904ea6cd52fa1da1e5e2871136f96
aqsa202 <aqsay202@gmail.com>
2025-04-03 07:04:59 +0000

    conversion

---
+++ 01-basics/conversion.js
@@ -0,0 +1,15 @@
+// let score = false
+// console.log(typeof score);
+//  const {score} req.body;
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+// "33"=33   ,  "33asd"=nan,   type of nan is number, true=1 false = 0 
+// let isLoggedIn = 1
+// let booleanisLoogedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoogedIn);
+let someNumber= 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);

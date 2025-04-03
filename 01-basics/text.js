3120c8befd7dda174ac529680b11daed7fedd9a1
aqsa202 <aqsay202@gmail.com>
2025-04-03 06:53:17 +0000

    text

---
+++ 01-basics/text.js
@@ -0,0 +1,16 @@
+const accountId = 445533;
+let accountEmail = "aqsay200@gmail.com";
+var accountPassward = "123456";
+let accountcity = "islamabad";
+// let accountState; // accountState is undefined
+
+// Updating values
+accountEmail = "jihan202@gmail.com";
+accountPassward = "7890";
+accountcity = "lahore";
+
+// Log accountId
+console.log(accountId);
+
+// Displaying data in a table format
+console.table([accountId, accountEmail, accountPassward, accountcity, ]);
